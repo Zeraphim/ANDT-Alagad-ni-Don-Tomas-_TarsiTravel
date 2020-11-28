312fe2d43cf3b4d39adf621b0af7f58f52587ee6
IllustrisInteractive <31766058+IllustrisInteractive@users.noreply.github.com>
2020-11-28 22:50:17 +0800

    Added University list to NavBar

---
+++ app/src/main/java/com/example/hackfest/UnivActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hackfest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnivActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.universities_page)
+    }
+}
