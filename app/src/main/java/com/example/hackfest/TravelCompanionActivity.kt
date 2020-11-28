ba1a92162523476b2085381b18b8ff1c07fe34b4
Zeraphim <jdmntec@gmail.com>
2020-11-28 22:58:45 +0800

    Update on overboard

---
+++ app/src/main/java/com/example/hackfest/TravelCompanionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hackfest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class TravelCompanionActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.travel_companion)
+    }
+}
